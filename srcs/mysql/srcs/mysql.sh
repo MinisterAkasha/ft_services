3ad11b909a7c48f1d8cfe9602bf8dd11823c8e44
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-04-25 16:45:51 +0300

    добавил редирект для wordpress из контейнера nginx

--- srcs/mysql/srcs/mysql.sh
+++ srcs/mysql/srcs/mysql.sh
@@ -6,7 +6,6 @@ touch /run/openrc/softlevel
 
 mysql -u root --skip-password -e "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
 mysql -u root --skip-password -e "CREATE USER 'root'@'%' IDENTIFIED BY '';"
-# mysql -u root --skip-password -e "grant all privileges on wordpress.* to root@% identified by '' with grant option;"
 mysql -u root --skip-password -e "grant all privileges on wordpress.* to 'root'@'%' WITH GRANT OPTION;"
 mysql -u root --skip-password -e "update mysql.user set plugin='' where user='root;"
 mysql -u root --skip-password -e "FLUSH PRIVILEGES;"
