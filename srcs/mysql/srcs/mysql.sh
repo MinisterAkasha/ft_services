50fc4fde0ee0583bdf62f8510433c30fb911a2f3
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-04-25 16:32:12 +0300

    добавил базу данных с тремя пользователями в контейнер mysql

--- srcs/mysql/srcs/mysql.sh
+++ srcs/mysql/srcs/mysql.sh
@@ -1,4 +1,3 @@
-
 # mysql_install_db
 openrc
 touch /run/openrc/softlevel
@@ -11,6 +10,8 @@ mysql -u root --skip-password -e "CREATE USER 'root'@'%' IDENTIFIED BY '';"
 mysql -u root --skip-password -e "grant all privileges on wordpress.* to 'root'@'%' WITH GRANT OPTION;"
 mysql -u root --skip-password -e "update mysql.user set plugin='' where user='root;"
 mysql -u root --skip-password -e "FLUSH PRIVILEGES;"
+
+mysql -u root wordpress < mnt/wordpress.sql
 
 /etc/init.d/mariadb stop
 
