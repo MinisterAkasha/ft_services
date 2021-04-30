1488f76a0ca40e383e4effcddd7bc187fb6f42fd
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-04-30 14:42:28 +0300

    убрал опечатку в setup.sh в пути к dockerfile wp

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ kubectl apply -f srcs/config_map.yaml
 
 
 docker build -t nginx_image ./srcs/nginx/
-docker build -t wordpress_image ./srcs/wp/
+docker build -t wordpress_image ./srcs/Wordpress/
 docker build -t mysql_image ./srcs/mysql/
 docker build -t php_my_admin_image srcs/phpMyAdmin
 docker build -t grafana_image srcs/Grafana
